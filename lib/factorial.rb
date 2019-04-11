74ba579528f76b600a210eab39dbf1da8210fe59
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-10 18:55:46 -0700

    Completed factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -2,5 +2,17 @@
 # Time complexity: ?
 # Space complexity: ?
 def factorial(number)
-  raise NotImplementedError
+  # raise NotImplementedError
+  if number == nil
+    raise ArgumentError
+  end
+
+  i = 1
+  while number > 0
+    i = number * i
+    number -= 1
+    # i += 1
+  end
+
+  return i
 end
